d9427e46ced517820b68f22910b310f1693ab3ee
saurabh-kumar-patel <111560254+saurabh-kumar-patel@users.noreply.github.com>
2026-06-26 08:17:55 +0000

    Deploying to gh-pages from @ saurabh-kumar-patel/life_sever@e950b412d577bf12171d9a495f30680ca30e2b88 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4122074964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1816314030" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
